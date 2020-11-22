c558097574c6b540185cb737af220872e776f9ce
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-22 08:49:02 -0800

    removed using statements

--- RecursivePatterns/Interfaces/IPerson.cs
+++ RecursivePatterns/Interfaces/IPerson.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RecursivePatterns.Interfaces
+﻿namespace RecursivePatterns.Interfaces
 {
     public interface IPerson
     {
