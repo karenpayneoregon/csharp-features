28f4cf72b07256d472d79f8b677b9489300ddeb8
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-07 07:25:31 -0800

    code sample for stored procedure with EF-Core

---
+++ EntityFrameworkCoreStoredProcedures/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using TempProject.Context;
+
+namespace StoredProcs1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void RunButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            await using var context = new NorthwindContext();
+            var test = new StoredProcedures(context);
+            var results = await test.CustomersByCountryIdentifier(12);
+
+            foreach (var result in results)
+            {
+                listBox1.Items.Add(result);
+            }
+
+        }
+    }
+}
---
+++ EntityFrameworkCoreStoredProcedures/readme.md
@@ -0,0 +1,3 @@
+﻿# About
+
+Simple example for executing a stored procedure where the stored procedure has joins. There are two methods, one to return all properties and a wrapper to return one property into a list.
