9b018ce1d6dad7052c623dbf06e53cd8d2663dd8
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-24 06:41:55 -0800

    HoHoHo

---
+++ HoHoHo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HoHoHo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random randomRow = new();
+            
+            int color, i = 0;
+            int height = randomRow.Next(6, 22);
+
+            int heightIncrement = height - 2;
+
+            var colors = (ConsoleColor[])Enum.GetValues(typeof(ConsoleColor));
+            var colorNumber = colors.Length;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("*".PadLeft(heightIncrement + 1));
+
+            while (i <= heightIncrement)
+            {
+                color = randomRow.Next(0, colorNumber);
+                Console.ForegroundColor = colors[color];
+                Console.WriteLine(new string('^', i++ * 2 + 1).PadLeft(heightIncrement + i));
+            }
+            
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(new string('|', 3).PadLeft(heightIncrement + 2));
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Merry Christmas and Happy New Year!");
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ HoHoHo/readme.md
@@ -0,0 +1,6 @@
+﻿# About
+
+Written in .NET Core 5, C# 9
+
+
+![screenshot](xmas.png)
