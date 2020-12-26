82c635f9a47f9ef2826d4f7d6d4648e8c3efd5b0
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-26 15:57:41 -0800

    Deconstruct record

---
+++ ConsoleAppTemp/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace DeconstructPerson
+{
+    class Program
+    {
+        private static void Main()
+        {
+            ShowRecordDeconstruction();
+
+            _ = ReadKey();
+        }
+
+        private static void ShowRecordDeconstruction()
+        {
+            var person = new Person("Karen", "Payne");
+
+            var (first, last) = person;
+            
+            WriteLine(first);
+            WriteLine(last);
+            
+        }
+    }
+
+    public record Person
+    {
+        
+        public string LastName { get; }
+        public string FirstName { get; }
+
+        public Person(string first, string last) => (FirstName, LastName) = (first, last);
+        public void Deconstruct(out string firstName, out string lastName) => (firstName, lastName) = (FirstName, LastName);
+        
+    } 
+}
