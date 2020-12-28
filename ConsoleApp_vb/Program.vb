e9032e7dd2433e08dd8851dd7ae40d8b3245b8dd
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-28 13:10:25 -0800

    refactors

--- ConsoleApp_vb/Program.vb
+++ ConsoleApp_vb/Program.vb
@@ -1,16 +1,21 @@
-Imports System
 Imports Microsoft.Extensions.DependencyInjection
 Imports Microsoft.Extensions.Logging
 
 Module Program
     Sub Main(args As String())
+
         Dim serviceCollection As New ServiceCollection()
         ConfigureServices(serviceCollection)
 
         Dim serviceProvider = serviceCollection.BuildServiceProvider()
 
+        Dim someClass = serviceProvider.GetService(Of SomeClass)()
+
+        someClass.LogInfo("Hello")
+
     End Sub
     Private Sub ConfigureServices(ByVal services As IServiceCollection)
+
         services.AddLogging(Function(configure) configure.AddConsole()).AddTransient(Of SomeClass)()
 
     End Sub
