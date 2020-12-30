5a14058e456ea07daddf2be972235e6777d79945
Karen Payne <Karen.1.Payne@oregon.gov>
2020-12-30 05:10:29 -0800

    added readme

--- ThreadExceptionWindowsFormsApp/Form1.Designer.cs
+++ ThreadExceptionWindowsFormsApp/Form1.Designer.cs
@@ -29,20 +29,36 @@ namespace ThreadExceptionWindowsFormsApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "See program.cs";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(422, 144);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unhandle exceptions at runtime logging";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ ThreadExceptionWindowsFormsApp/readme.md
@@ -0,0 +1,32 @@
+﻿# About
+
+Simple code sample for application-wide exception handler to write unhandled exceptions to a log file.
+
+# Prerequisites
+
+To run this project *as is* in this repository.
+
+- VS2019
+- .NET Core 5
+
+
+# Get source without all projects 
+
+With Git installed, create a batch file with the following contents, save, run and add the cloned projects to your solution.
+```batch
+mkdir code
+cd code
+git init
+git remote add -f origin https://github.com/karenpayneoregon/csharp-features
+git sparse-checkout init --cone
+git sparse-checkout add ExceptionHandling
+git sparse-checkout add ThreadExceptionWindowsFormsApp
+git pull origin master
+:clean-up
+del .gitattributes
+del .gitignore
+del .yml
+del .editorconfig
+del *.md
+del *.sln
+```
