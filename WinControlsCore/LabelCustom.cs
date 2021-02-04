c7e8e4b2d5000fb9a89a58f32cb51371cb500372
Karen Payne <Karen.1.Payne@oregon.gov>
2021-02-04 10:51:16 -0800

    Work in progress

---
+++ WinControlsCore/LabelCustom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinControlsCore
+{
+    public class LabelCustom : Label
+    {
+        [Category("Behavior"), Description("Identifier")]
+        public int? Id { get; set; }
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public bool HasId => Id.HasValue;
+        [Category("Behavior"), Description("Comment")]
+        public string Comment { get; set; }
+    }
+}
