53a51170aeded7bc50d712a3be568ba4bf98f395
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-23 10:56:54 -0700

    removed trash code

--- SwitchExpression/Program.cs
+++ SwitchExpression/Program.cs
@@ -8,21 +8,8 @@ namespace SwitchExpressionStackOverflow
         {
             var projet = new ProjectItem() { IdPlateforme = 1, IdPlateformeSecteur = 2 };
             projet.IdSecteur = Operations.SetIdSecteur(projet);
+            
             Debug.WriteLine($"projet.IdPlateformeSecteur = {projet.IdPlateformeSecteur}");
-
-            projet.IdPlateformeSecteur = projet.IdPlateforme switch
-            {
-                1 when projet.IdSecteur == 1 => 1,
-                1 when projet.IdSecteur == 2 => 2,
-                1 when projet.IdSecteur == 4 => 3,
-                3 when projet.IdSecteur == 1 => 4,
-                3 when projet.IdSecteur == 2 => 5,
-                3 when projet.IdSecteur == 4 => 6,
-                2 when projet.IdSecteur == 1 => 7,
-                2 when projet.IdSecteur == 2 => 8,
-                2 when projet.IdSecteur == 4 => 9,
-                _ => projet.IdPlateformeSecteur
-            };
 
 
 
