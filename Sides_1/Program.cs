1301b105b2071dd0b92c7656afcc54ed9be517e1
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-27 14:08:35 -0700

    Code to assist with MPC

---
+++ Sides_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sides_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            DifferencesBetweenTextFiles();
+            Console.ReadKey();
+        }
+
+
+        private static void DifferencesBetweenTextFiles()
+        {
+            const string folderName = "C:\\OED\\Documents\\Pulls";
+            
+            var foundList = File.ReadAllLines("Email.txt")
+                .ToList()
+                .Except(File.ReadAllLines("Excel.txt").ToList())
+                .ToList();
+
+            if (foundList.Count <= 0) return;
+            
+            foreach (var fileName in foundList
+                .Select(guid => Directory.GetFiles(folderName, $"{guid}*.pdf"))
+                .SelectMany(result => result)) { Console.WriteLine(fileName); }
+
+
+
+        }
+
+    }
+}
